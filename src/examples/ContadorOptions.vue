26306fd03f42e6f092b750db784c65688c9e6d09
Edimer C <desarrollowedimer@gmail.com>
2025-03-09 12:57:44 -0500

    add basic examples whith Options API and Composition API

---
+++ src/examples/ContadorOptions.vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+export default {
+    data() {
+        return{
+            count: 0
+        }
+    },
+    methods: {
+        increase () {
+            this.count += 1
+        }
+    },
+    mounted() {
+        
+    }
+
+}
+
+</script>
+
+<template>
+    <h1>contador: {{ count }}</h1>
+    <button @click="increase">Aumentar</button>
+</template>
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,7 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 
-import App from './App.vue'
-import router from './router'
+import Contador from './examples/ContadorCompositions.vue'
 
-const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
+const app = createApp(Contador)
 
 app.mount('#app')
