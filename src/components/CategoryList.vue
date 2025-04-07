4a6f59b1614a7ef5047440ad7e5f9ba40730e79b
Edimer C <desarrollowedimer@gmail.com>
2025-04-06 21:46:27 -0500

    Add images for products, Add categotyId

--- src/components/CategoryList.vue
+++ src/components/CategoryList.vue
@@ -5,10 +5,28 @@ import type { Category } from '@/model/types';
             return{
                 categories: [{
                     id:1,
-                    name: 'Technology'
+                    name: 'Colagenos',
+                    description: 'Descubre nuestra variedad de colagenos como el hidrolizado y los tipo 1, 2 y 3'
                 },{
                     id:2,
-                    name: 'Toys'
+                    name: 'Defensas',
+                    description: 'Descubre nuestra variedad de productos para elevar tus defensas y estar siempre saludable'
+                },{
+                    id:3,
+                    name: 'Deporte',
+                    description: 'Descubre nuestra variedad de suplementos deportivos y empieza a rendir al máximo'
+                },{
+                    id:4,
+                    name: 'Cardiovascular',
+                    description: 'Descubre nuestra variedad de productos para el cuidado del corazon'
+                },{
+                    id:5,
+                    name: 'Antioxidante',
+                    description: 'Descubre nuestra variedad de productos para neutralizar radicales libres y retrasar el daño celular'
+                },{
+                    id:6,
+                    name: 'Vitaminas',
+                    description: 'Descubre nuestra variedad de vitaminas a corde a tu necesidad'
                 }] as Category[]
             }
         }
--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -12,24 +12,30 @@ export default {
         return{
             products: [
                 {name: 'Colageno', price: 75000, id: 1, 
-                    image:'https://acdn-us.mitiendanube.com/stores/232/602/products/edercolag-edervit1-db2123f490da9b6b3515832699991329-640-0.png'
+                    image:'/products/colageno.png',
+                    categoryId: 1
                 },
                 {name: 'Proteina',price: 160000, id: 2, 
-                    image:'https://www.levennatural.com/wp-content/uploads/2021/09/GMN14.jpg'
+                    image:'/products/proteina.webp',
+                    categoryId: 3
                 },
                 {name: 'Calcio', price: 57000, id: 3, 
-                    image:'https://cdnx.jumpseller.com/mis-vitaminas/image/3743696/resize/1200/1200?1654967356'
+                    image:'/products/calcio.webp',
+                    categoryId: 1
                 },
                 {name: 'Calostro', price: 65000, id: 4, 
-                    image:'https://molinatural.com/wp-content/uploads/2021/02/Diseno-sin-titulo-29-1.png'
+                    image:'/products/calostro.webp',
+                    categoryId: 2
                 },
                 {name: 'Te verde',price: 32000, id: 5, 
-                    image:'https://cdnx.jumpseller.com/mis-vitaminas/image/8278259/resize/1200/1200?1651951340'
+                    image:'/products/TeVerde.webp',
+                    categoryId: 5
                 }, 
-                {name: 'Omega 3',price: 52500, id: 5, 
-                    image:'https://dexter-media-prod-co.s3.amazonaws.com/__sized__/products/__filtered__/79078603__webp__-thumbnail-1080x1080-70.webp'
+                {name: 'Omega 3',price: 52500, id: 6, 
+                    image:'/products/omega.webp',
+                    categoryId: 4
                 },   
-                {name: 'Mi vitamina',price: 57300, id: 5},               
+                {name: 'Mi vitamina',price: 57300, id: 7 ,categoryId: 6},               
             ] as Product[]
         }
     },
