112f835069481fd68d8aa9dd3623edeb4c49aa4d
Edimer C <desarrollowedimer@gmail.com>
2025-04-16 22:00:24 -0500

    Move json files to src/data if we want to import

--- src/stores/categories.ts
+++ src/stores/categories.ts
@@ -1,33 +1,10 @@
 import type { Category } from '../model/types';
 import { defineStore } from 'pinia';
+import categoriesData from '../data/categories.json'
 
 export const useCategoriesStore = defineStore('categories', {
     state: () => ({
-        categories: [{
-            id:1,
-            name: 'Colagenos',
-            description: 'Descubre nuestra variedad de colagenos como el hidrolizado y los tipo 1, 2 y 3'
-        },{
-            id:2,
-            name: 'Defensas',
-            description: 'Descubre nuestra variedad de productos para elevar tus defensas y estar siempre saludable'
-        },{
-            id:3,
-            name: 'Deporte',
-            description: 'Descubre nuestra variedad de suplementos deportivos y empieza a rendir al máximo'
-        },{
-            id:4,
-            name: 'Cardiovascular',
-            description: 'Descubre nuestra variedad de productos para el cuidado del corazon'
-        },{
-            id:5,
-            name: 'Antioxidante',
-            description: 'Descubre nuestra variedad de productos para neutralizar radicales libres y retrasar el daño celular'
-        },{
-            id:6,
-            name: 'Vitaminas',
-            description: 'Descubre nuestra variedad de vitaminas a corde a tu necesidad'
-        }] as Category[]
+        categories: categoriesData as Category[]
     }),
     getters: {
   
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
-import { createHtmlPlugin } from 'vite-plugin-html'
+//import { createHtmlPlugin } from 'vite-plugin-html'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
